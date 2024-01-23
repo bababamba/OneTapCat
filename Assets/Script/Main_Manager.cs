3547d1d9e44cc70956cbfe212b93c650dc14c9b2
bababamba <86463743+bababamba@users.noreply.github.com>
2024-01-23 14:43:37 +0900

    update

--- Assets/Script/Main_Manager.cs
+++ Assets/Script/Main_Manager.cs
@@ -12,22 +12,27 @@ public class Main_Manager : MonoBehaviour
     [SerializeField] Image Right_Sprite;
     [SerializeField] Image Left_Sprite;
 
-    [SerializeField] Image[] Main_Image;
-    [SerializeField] Image[] Right_Image;
-    [SerializeField] Image[] Left_Image;
-    [SerializeField] Image[] Ending_Image;
-    [SerializeField] Image[] Fail_Image;
+    [SerializeField] Sprite[] Main_Image;
+    [SerializeField] Sprite[] Right_Image;
+    [SerializeField] Sprite[] Left_Image;
+    [SerializeField] Sprite[] Ending_Image;
+    [SerializeField] Sprite[] Fail_Image;
 
-    
+    [SerializeField] GameObject Endcard;
+    [SerializeField] GameObject MainGame;
+    [SerializeField] GameObject StageSelect;
+
 
     int stage = 0;
     int progress = 0;
-
+    int EndA = 0;
+    int EndB = 0;
+    int[] data = {0,5,1,2,3,4,6,0};
 
     // Start is called before the first frame update
     void Start()
     {
-        stage = 1;progress = 1;// 임시
+        stage = 1;progress = 0;// 임시
     }
 
     // Update is called once per frame
@@ -35,17 +40,143 @@ public class Main_Manager : MonoBehaviour
     {
         
     }
-    public void UpdateScreen(int stageNumber, int progressNumber) {
-        Main_Sprite = Main_Image[(stageNumber - 1) * 4 + progressNumber];
-        Right_Sprite = Right_Image[(stageNumber - 1) * 4 + progressNumber];
-        Left_Sprite = Left_Image[(stageNumber - 1) * 4 + progressNumber];
-
-
-
-
+    public void UpdateScreen(int progressNumber) {
+        Debug.Log(progress);
+        Main_Sprite.sprite = Main_Image[(stage - 1) * 4 + progressNumber];
+        Right_Sprite.sprite = Right_Image[(stage - 1) * 4 + progressNumber];
+        Left_Sprite.sprite = Left_Image[(stage - 1) * 4 + progressNumber];
+    }
+    public void FailScreen(int endNumber)
+    {
+        Main_Sprite.sprite = Fail_Image[(stage - 1) * 2 + endNumber-1];
+        Endcard.SetActive(true);
 
     }
+    int Ending()
+    {
+        int temp = 0;
+        if (EndA == 1)
+        {
+            if (EndB == 1)
+                temp = 1;
+            else
+                temp = 2;
+        }
+        else
+        {
+            if (EndB == 1)
+                temp = 3;
+            else
+                temp = 4;
+        }
 
+
+        return temp;
+    }
+    public void EndingScreen(int endNumber)
+    {
+        Main_Sprite.sprite = Ending_Image[(stage - 1) * 4 + endNumber-1];
+        Endcard.SetActive(true);
+
+    }
+    public void ButtonLeft()
+    {
+        
+        
+        if (data[progress * 2] == 5)
+        {
+            FailScreen(1);
+        }
+        else if (data[progress * 2] == 6)
+        {
+            FailScreen(2);
+        }
+        else if (data[progress * 2] == 1)
+        {
+            EndA = 1;
+            progress++;
+            if (progress == 4)
+                EndingScreen(Ending());
+            else
+                UpdateScreen(progress);
+        }
+        else if (data[progress * 2] == 3)
+        {
+            EndB = 1;
+            progress++;
+            if (progress == 4)
+                EndingScreen(Ending());
+            else
+                UpdateScreen(progress);
+        }
+        else if (data[progress * 2] == 0)
+        {
+            progress++;
+            if (progress == 4)
+                EndingScreen(Ending());
+            else
+                UpdateScreen(progress);
+        }
+        
+    }
+    public void ButtonRight()
+    {
+        if (progress == 4)
+            EndingScreen(Ending());
+        else
+        {
+            if (data[progress * 2+1] == 5)
+            {
+                FailScreen(1);
+            }
+            else if (data[progress * 2 + 1] == 6)
+            {
+                FailScreen(2);
+            }
+            else if (data[progress * 2 + 1] == 2)
+            {
+                EndA = 2;
+                progress++;
+                if (progress == 4)
+                    EndingScreen(Ending());
+                else
+                    UpdateScreen(progress);
+            }
+            else if (data[progress * 2 + 1] == 4)
+            {
+                EndB = 2;
+                progress++;
+                if (progress == 4)
+                    EndingScreen(Ending());
+                else
+                    UpdateScreen(progress);
+            }
+            else if (data[progress * 2 + 1] == 0)
+            {
+                progress++;
+                if (progress == 4)
+                    EndingScreen(Ending());
+                else
+                    UpdateScreen(progress);
+            }
+        }
+    }
+
+    public void CloseAll()
+    {
+        StageSelect.SetActive(false);
+        MainGame.SetActive(false);
+    }
+    public void OpenMainGame()
+    {
+        CloseAll();
+        MainGame.SetActive(true);
+    }
+    public void OpenStageSelect()
+    {
+        CloseAll();
+        StageSelect.SetActive(true);
+    }
 
 
 
