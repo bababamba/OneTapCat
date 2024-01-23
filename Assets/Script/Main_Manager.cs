cc1c3a7ac24288cb159d1c9c3a65671a5e00840d
bababamba <86463743+bababamba@users.noreply.github.com>
2024-01-24 02:30:51 +0900

    update stamina+stage 1

--- Assets/Script/Main_Manager.cs
+++ Assets/Script/Main_Manager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Main_Manager : MonoBehaviour
 {
@@ -18,9 +19,16 @@ public class Main_Manager : MonoBehaviour
     [SerializeField] Sprite[] Ending_Image;
     [SerializeField] Sprite[] Fail_Image;
 
+    [SerializeField] TMP_Text UpperMessage;
     [SerializeField] GameObject Endcard;
+    [SerializeField] TMP_Text EndMessage;
+    [SerializeField] GameObject Failcard;
+    [SerializeField] TMP_Text FailMessage;
     [SerializeField] GameObject MainGame;
-    [SerializeField] GameObject StageSelect;
+    [SerializeField] GameObject SelectScreen;
+    [SerializeField] GameObject[] StageSelect;
+    [SerializeField] StaminaManager stamina;
+    
 
 
     int stage = 0;
@@ -28,6 +36,17 @@ public class Main_Manager : MonoBehaviour
     int EndA = 0;
     int EndB = 0;
     int[] data = {0,5,1,2,3,4,6,0};
+    string[] script = { 
+        "\"냐아앙..\" \n지각해서 혼났다. 5분만 더 잔다는 게 30분이나 자버렸다니…", 
+        "\"나 좀 도와조라냥..\" \n움직일수록 더 꼬여! 실을 가지고 놀다 몸에 엉켜버렸다.",
+        "\"가지고 있는 생선 다 내놔!!\" \n불량식품을 즐겨먹다가 불량 냥이가 되어버렸다. ",
+        "\"저.. 저기..\" \n친구를 사귀고 싶지만, 말 걸기는 무서워… 귀여운 부끄 냥이가 됐다!",
+        "\"여기 여기 모여라! 나랑 놀자냥!\" \n모두가 좋아하는 인싸 냥이가 됐다! ",
+        "\"모르는 게 없다냥\" \n공부도 체육도 1등! 못하는 게 없는 전교 1등 냥이가 됐다!",
+    };
+    string[] quest = { "아침이다.. 너무 피곤한데 어떡하지?","학교 가기 전 문방구에서 뭐 사지?","드디어 쉬는 시간이다! 나는 뭘 할까?","체육 시간에 어떤 운동을 할까?",
+        "지각한 냥이","실에 걸린 냥이",
+        "골목대장 냥아치","소심한 부끄 냥이","인기 스타 인싸 냥이","전교 1등 모범 냥이" };
 
     // Start is called before the first frame update
     void Start()
@@ -42,6 +61,7 @@ public class Main_Manager : MonoBehaviour
     }
     public void UpdateScreen(int progressNumber) {
         Debug.Log(progress);
+        UpperMessage.text = quest[progress];
         Main_Sprite.sprite = Main_Image[(stage - 1) * 4 + progressNumber];
         Right_Sprite.sprite = Right_Image[(stage - 1) * 4 + progressNumber];
         Left_Sprite.sprite = Left_Image[(stage - 1) * 4 + progressNumber];
@@ -49,7 +69,10 @@ public class Main_Manager : MonoBehaviour
     public void FailScreen(int endNumber)
     {
         Main_Sprite.sprite = Fail_Image[(stage - 1) * 2 + endNumber-1];
-        Endcard.SetActive(true);
+        
+        Failcard.SetActive(true);
+        UpperMessage.text = quest[3 + endNumber];
+        FailMessage.text = script[endNumber - 1];
 
     }
     int Ending()
@@ -76,8 +99,16 @@ public class Main_Manager : MonoBehaviour
     public void EndingScreen(int endNumber)
     {
         Main_Sprite.sprite = Ending_Image[(stage - 1) * 4 + endNumber-1];
+        EndMessage.text = script[endNumber + 1];
+        UpperMessage.text = quest[5 + endNumber];
         Endcard.SetActive(true);
 
+    }
+    public void Retry()
+    {
+        progress = 0;
+        Failcard.SetActive(false);
+        UpdateScreen(progress);
     }
     public void ButtonLeft()
     {
@@ -164,7 +195,10 @@ public class Main_Manager : MonoBehaviour
 
     public void CloseAll()
     {
-        StageSelect.SetActive(false);
+
+        SelectScreen.SetActive(false);
+        Failcard.SetActive(false);
+        Endcard.SetActive(false);
         MainGame.SetActive(false);
     }
     public void OpenMainGame()
@@ -172,10 +206,18 @@ public class Main_Manager : MonoBehaviour
         CloseAll();
         MainGame.SetActive(true);
     }
-    public void OpenStageSelect()
+    public void OpenStageSelect(int select)
     {
         CloseAll();
-        StageSelect.SetActive(true);
+        stage = select;
+        StageSelect[select-1].SetActive(true);
+    }
+    public void OpenStagesSelectScreen()
+    {
+        CloseAll();
+        progress = 0;
+        UpdateScreen(0);
+        SelectScreen.SetActive(true);
     }
 
 
---
+++ Assets/Script/StaminaManager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StaminaManager : MonoBehaviour
+{
+    //public static StaminaManager stamina;
+    [SerializeField] TMP_Text time_text;
+    [SerializeField] TMP_Text stamina_text;
+
+    int CurStamina = 50;
+    static int MaxStamina = 100;
+
+    float CurStaminaTime = 30f;
+    static float MaxStaminaTime = 60f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CurStamina <= MaxStamina)
+        {
+            CurStaminaTime -= Time.deltaTime;
+            UpdateDisplay();
+        }
+        if (CurStaminaTime <= 0)
+        {
+            CurStaminaTime = MaxStaminaTime;
+            //임시 자동 스테미나 충전량
+            StaminaUp(10);
+            UpdateDisplay();
+        }
+    }
+    //스테미나 증가 시에 사용
+    public void StaminaUp(int value)
+    {
+        CurStamina += value;
+        if (CurStamina > MaxStamina)
+        {
+            CurStamina = MaxStamina;
+            CurStaminaTime = MaxStaminaTime;
+            UpdateDisplay();
+        }
+    }
+    //스테미나 감소시, 감소하였을 때에 0 미만인지 체크
+    public bool StaminaCheck(int value)
+    {
+        if (CurStamina - value > 0f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    //스테미나 감소 시 위 함수와 함께 사용
+    public void StaminaDown(int value)
+    {   
+            CurStamina -= value;
+    }
+    //현재 스테미나 값
+    public int GetStamina()
+    {
+        return CurStamina;
+    }
+    public void SetStamina(int value)
+    {
+        CurStamina = value;
+        if (CurStamina > MaxStamina)
+            CurStamina = MaxStamina;
+        UpdateDisplay();
+    }
+
+    //현재 스테미나 충전 시간
+    public float GetStaminaTime()
+    {
+        return CurStaminaTime;
+    }
+    //현재 스테미나 충전시간 설정
+    public void SetStaminaTime(float time)
+    {
+        CurStaminaTime = time;
+    }
+    public void UpdateDisplay()
+    {
+        stamina_text.text = CurStamina.ToString();
+        time_text.text = ((int)CurStaminaTime / 60).ToString() +  ":" + ((int)CurStaminaTime % 60).ToString();
+    } 
+
+}
